5d33a277e2a5b01f89c4c2c59a740d5635f6d5ce
lbreede <67719520+lbreede@users.noreply.github.com>
2025-07-01 19:13:36 -0700

    Comment out temporary token printing code in Lox class

--- java/com/craftinginterpreters/lox/Lox.java
+++ java/com/craftinginterpreters/lox/Lox.java
@@ -48,6 +48,11 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
+        // // For now, just print the tokens.
+        // for (Token token : tokens) {
+        // System.out.println(token);
+        // }
+
         Parser parser = new Parser(tokens);
         Expr expression = parser.parse();
 
